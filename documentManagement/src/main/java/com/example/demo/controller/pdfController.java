b1aded6b97d25432171e3622f61feba906623b93
ahdharmadhikari@gmail.com <Abhi@98230>
2025-10-31 18:01:55 +0530

    add method in controller/pdfController.java

--- documentManagement/src/main/java/com/example/demo/controller/pdfController.java
+++ documentManagement/src/main/java/com/example/demo/controller/pdfController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class pdfController {
 
+	public void m1() {
+		
+		System.out.println("hellao abhishek");
+	}
+	
 }
