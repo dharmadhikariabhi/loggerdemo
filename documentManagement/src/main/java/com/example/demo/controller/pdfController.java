b3e7f707e1259b24573370f8b22de4e706414b19
ahdharmadhikari@gmail.com <Abhi@98230>
2025-10-31 18:03:11 +0530

    nothing

--- documentManagement/src/main/java/com/example/demo/controller/pdfController.java
+++ documentManagement/src/main/java/com/example/demo/controller/pdfController.java
@@ -5,9 +5,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class pdfController {
 
-	public void m1() {
-		
-		System.out.println("hellao abhishek");
-	}
-	
+
 }
